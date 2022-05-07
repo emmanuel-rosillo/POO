15f06d7ae7252be8ea3a5725dd13e0c573fd01e3
emmanuel-rosillo <72666912+emmanuel-rosillo@users.noreply.github.com>
2022-05-06 19:37:05 -0500

    add readme and images to the readme

--- POOp11/Readme.md
+++ POOp11/Readme.md
@@ -1,1 +1,41 @@
-# Un saludo 
+# Los lectores y escritores de ficheros externos en java
+
+
+<center>
+<h1>
+<font color = #00FFFF>
+file writer and buffered write
+</font>
+</h1>
+</center>
+
+# method "exist()"
+
+![Texto alternativo](/POOp11/images/is.png)
+
+### return boolean value
+# Class FileOutputStream  
+![Texto alternativo](/POOp11/images/ll.png)
+
+### write a file limited to a certain number of bytes  
+
+# Class FileInputStream
+
+![Texto alternativo](/POOp11/images/op.png)
+### use arraybuffer object to read file in java
+
+# Class BufferedReader
+![Texto alternativo](/POOp11/images/oi.png)
+### used to read a file in a unlimited ArrayBuffered
+# FileReader
+![Texto alternativo](/POOp11/images/tt.png)
+### read a input file
+# Class StringTokenizer
+![Texto alternativo](/POOp11/images/tk.png)
+### separe the content on input file with a delimiter
+# Examples:
+## this additional activity, stores a studens objets in Linked List, using input file to asiggnament values in studens object
+
+![Texto alternativo](/POOp11/images/ac.png)
+
+
--- POOp11/src/javaapplication1/JavaApplication1.java
+++ POOp11/src/javaapplication1/JavaApplication1.java
@@ -21,117 +21,116 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        System.out.println();
-//        File f1 = new File ("archivo.txt");
-//        System.out.println(f1.exists());
-//        if(!f1.exists()){
-//            try{
-//                boolean n = f1.createNewFile();
-//            }catch(IOException a){
-//                System.out.println(a.getMessage());
-//            }
-//        }
-//        FileOutputStream fos = null;
-//        byte[] buffer = new byte[81];
-//        int n;
-//        System.out.println("escriba texto");
-//        try{
-//            n = System.in.read(buffer);
-//            fos = new FileOutputStream("fos.txt");
-//            fos.write(buffer, 0, n);
-//        } catch (IOException ex) {
-//            Logger.getLogger(ex.getMessage());
-//        }finally{
-//            if(fos != null){
-//                try{
-//                    fos.close();
-//                } catch (IOException ex) {
-//                    Logger.getLogger(ex.getMessage());
-//                }
-//            }
-//        }
-//        FileInputStream fis = null;
-//        try{
-//            fis= new FileInputStream("fos.txt");
-//            n = fis.readNBytes(buffer, 0, buffer.length);
-//            String t = new String(buffer, 0, n);
-//            System.out.println(t);
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-//        }catch (IOException ex) {
-//                    Logger.getLogger(ex.getMessage());
-//        }finally{
-//            if(fis!=null){
-//                try{
-//                    fis.close();
-//                } catch (IOException ex) {
-//                    Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//            }
-//        }
-//        System.out.println("···buffer···");
-//        BufferedReader b1 = new BufferedReader(new InputStreamReader(System.in));
-//        System.out.println("esciba");
-//        try{
-//            String t1 = b1.readLine();
-//            FileWriter fw = new FileWriter("fw.csv");
-//            BufferedWriter bw = new BufferedWriter(fw);
-//            PrintWriter salida = new PrintWriter(bw);
-//            salida.println(t1);
-//            for (int i = 0; i < 10; i++) {
-//                salida.println("line" + i);
-//            }
-//            for (int i = 0; i < 10; i++) {
-//                salida.println("elemma, uvas, 31255566" + i);
-//
-//            }
-//            salida.close();
-//        } catch (IOException ex) {
-//            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-//        }finally{
-//
-//        }
-//        System.out.println("···reader···");
-//        try{
-//
-//            FileReader fr = new FileReader("fw.csv");
-//            b1 = new BufferedReader(fr);
-//            System.out.println("texto: ");
-//            String l = b1.readLine();
-//            while(l != null){
-//                System.out.println(l);
-//                l = b1.readLine();
-//            }
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (IOException ex) {
-//            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        System.out.println("#####StringTokenizer####");
-//        String c1 = "elemma, uvas, 312555660";
-//        StringTokenizer tiktok = new StringTokenizer(c1, ",");
-//        while(tiktok.hasMoreTokens()){
-//            System.out.println(tiktok.nextToken());
-//        }
-//        System.out.println("¨¨¨¨¨¨¨ actividades extra ¨¨¨¨¨¨¨");
-//        System.out.println("\n");
-//        System.out.println("Usar tokenizer en FileReader");
-//        System.out.println("########");
-//        try{
-//            FileReader f2 = new FileReader("archivo.txt");
-//            BufferedReader b2 = new BufferedReader(f2);
-//            System.out.println("texto: ");
-//            String p = b2.readLine();
-//            StringTokenizer tok = new StringTokenizer(p, ",");
-//            while(tok.hasMoreTokens()){
-//                System.out.println(tok.nextToken());
-//            }
-//            System.out.println("########");
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (IOException ex) {
-//            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        System.out.println();
+        File f1 = new File ("archivo.txt");
+        System.out.println(f1.exists());
+        if(!f1.exists()){
+            try{
+                boolean n = f1.createNewFile();
+            }catch(IOException a){
+                System.out.println(a.getMessage());
+            }
+        }
+        FileOutputStream fos = null;
+        byte[] buffer = new byte[81];
+        int n;
+        System.out.println("escriba texto");
+        try{
+            n = System.in.read(buffer);
+            fos = new FileOutputStream("fos.txt");
+            fos.write(buffer, 0, n);
+        } catch (IOException ex) {
+            Logger.getLogger(ex.getMessage());
+        }finally{
+            if(fos != null){
+                try{
+                    fos.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(ex.getMessage());
+                }
+            }
+        }
+        FileInputStream fis = null;
+        try{
+            fis= new FileInputStream("fos.txt");
+            n = fis.readNBytes(buffer, 0, buffer.length);
+            String t = new String(buffer, 0, n);
+            System.out.println(t);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }catch (IOException ex) {
+                    Logger.getLogger(ex.getMessage());
+        }finally{
+            if(fis!=null){
+                try{
+                    fis.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        System.out.println("···buffer···");
+        BufferedReader b1 = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("esciba");
+        try{
+            String t1 = b1.readLine();
+            FileWriter fw = new FileWriter("fw.csv");
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter salida = new PrintWriter(bw);
+            salida.println(t1);
+            for (int i = 0; i < 10; i++) {
+                salida.println("line" + i);
+            }
+            for (int i = 0; i < 10; i++) {
+                salida.println("elemma, uvas, 31255566" + i);
+
+            }
+            salida.close();
+        } catch (IOException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+
+        }
+        System.out.println("···reader···");
+        try{
+            FileReader fr = new FileReader("fw.csv");
+            b1 = new BufferedReader(fr);
+            System.out.println("texto: ");
+            String l = b1.readLine();
+            while(l != null){
+                System.out.println(l);
+                l = b1.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("#####StringTokenizer####");
+        String c1 = "elemma, uvas, 312555660";
+        StringTokenizer tiktok = new StringTokenizer(c1, ",");
+        while(tiktok.hasMoreTokens()){
+            System.out.println(tiktok.nextToken());
+        }
+        System.out.println("¨¨¨¨¨¨¨ actividades extra ¨¨¨¨¨¨¨");
+        System.out.println("\n");
+        System.out.println("Usar tokenizer en FileReader");
+        System.out.println("########");
+        try{
+            FileReader f2 = new FileReader("archivo.txt");
+            BufferedReader b2 = new BufferedReader(f2);
+            System.out.println("texto: ");
+            String p = b2.readLine();
+            StringTokenizer tok = new StringTokenizer(p, ",");
+            while(tok.hasMoreTokens()){
+                System.out.println(tok.nextToken());
+            }
+            System.out.println("########");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
         System.out.println("~~~~~~~ almacen de objetos ~~~~~~~");
         LinkedList<Alumno> alum = new LinkedList<>();
         try{
